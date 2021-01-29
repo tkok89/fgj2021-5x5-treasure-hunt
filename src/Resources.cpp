d675ff06b1de95be645fbe8bd9ddc9df40a257d5
Janne Hiiskoski <janne@hiiskoski.fi>
2021-01-30 00:17:43 +0200

    Name sound resources

--- src/Resources.cpp
+++ src/Resources.cpp
@@ -25,51 +25,67 @@ Resources::Resources()
         std::cerr << "Error loading Vera.ttf" << std::endl;
     }
 
-    success = soundEffects[0].loadFromFile(resourcePath() + "assets/sounds/bonk.ogg");
+    success = soundEffects[SoundResourceName::bonk]
+        .loadFromFile(resourcePath() + "assets/sounds/bonk.ogg");
     assert(success);
 
-    success = soundEffects[1].loadFromFile(resourcePath() + "assets/sounds/break.ogg");
+    success = soundEffects[SoundResourceName::break_]
+        .loadFromFile(resourcePath() + "assets/sounds/break.ogg");
     assert(success);
 
-    success = soundEffects[2].loadFromFile(resourcePath() + "assets/sounds/button_press.ogg");
+    success = soundEffects[SoundResourceName::button_press]
+        .loadFromFile(resourcePath() + "assets/sounds/button_press.ogg");
     assert(success);
 
-    success = soundEffects[3].loadFromFile(resourcePath() + "assets/sounds/gameover.ogg");
+    success = soundEffects[SoundResourceName::gameover]
+        .loadFromFile(resourcePath() + "assets/sounds/gameover.ogg");
     assert(success);
 
-    success = soundEffects[4].loadFromFile(resourcePath() + "assets/sounds/miu.ogg");
+    success = soundEffects[SoundResourceName::miu]
+        .loadFromFile(resourcePath() + "assets/sounds/miu.ogg");
     assert(success);
 
-    success = soundEffects[5].loadFromFile(resourcePath() + "assets/sounds/mjau.ogg");
+    success = soundEffects[SoundResourceName::mjau]
+        .loadFromFile(resourcePath() + "assets/sounds/mjau.ogg");
     assert(success);
 
-    success = soundEffects[6].loadFromFile(resourcePath() + "assets/sounds/monster_ded.ogg");
+    success = soundEffects[SoundResourceName::monster_ded]
+        .loadFromFile(resourcePath() + "assets/sounds/monster_ded.ogg");
     assert(success);
 
-    success = soundEffects[7].loadFromFile(resourcePath() + "assets/sounds/mouse_hit_wall.ogg");
+    success = soundEffects[SoundResourceName::mouse_hit_wall]
+        .loadFromFile(resourcePath() + "assets/sounds/mouse_hit_wall.ogg");
     assert(success);
 
-    success = soundEffects[8].loadFromFile(resourcePath() + "assets/sounds/mouse_mus_mus.ogg");
+    success = soundEffects[SoundResourceName::mouse_mus_mus]
+        .loadFromFile(resourcePath() + "assets/sounds/mouse_mus_mus.ogg");
     assert(success);
 
-    success = soundEffects[9].loadFromFile(resourcePath() + "assets/sounds/movetiles_1.ogg");
+    success = soundEffects[SoundResourceName::movetiles_1]
+        .loadFromFile(resourcePath() + "assets/sounds/movetiles_1.ogg");
     assert(success);
 
-    success = soundEffects[10].loadFromFile(resourcePath() + "assets/sounds/movetiles_2.ogg");
+    success = soundEffects[SoundResourceName::movetiles_2]
+        .loadFromFile(resourcePath() + "assets/sounds/movetiles_2.ogg");
     assert(success);
 
-    success = soundEffects[11].loadFromFile(resourcePath() + "assets/sounds/movetiles_3.ogg");
+    success = soundEffects[SoundResourceName::movetiles_3]
+        .loadFromFile(resourcePath() + "assets/sounds/movetiles_3.ogg");
     assert(success);
 
-    success = soundEffects[12].loadFromFile(resourcePath() + "assets/sounds/o_oui.ogg");
+    success = soundEffects[SoundResourceName::o_oui]
+        .loadFromFile(resourcePath() + "assets/sounds/o_oui.ogg");
     assert(success);
 
-    success = soundEffects[13].loadFromFile(resourcePath() + "assets/sounds/peepeep.ogg");
+    success = soundEffects[SoundResourceName::peepeep]
+        .loadFromFile(resourcePath() + "assets/sounds/peepeep.ogg");
     assert(success);
 
-    success = soundEffects[14].loadFromFile(resourcePath() + "assets/sounds/togglebutton_1.ogg");
+    success = soundEffects[SoundResourceName::togglebutton_1]
+        .loadFromFile(resourcePath() + "assets/sounds/togglebutton_1.ogg");
     assert(success);
 
-    success = soundEffects[15].loadFromFile(resourcePath() + "assets/sounds/togglebutton_2.ogg");
+    success = soundEffects[SoundResourceName::togglebutton_2]
+        .loadFromFile(resourcePath() + "assets/sounds/togglebutton_2.ogg");
     assert(success);
 }
--- src/Resources.h
+++ src/Resources.h
@@ -5,6 +5,26 @@
 #include <SFML/Graphics/Font.hpp>
 #include <SFML/Audio/SoundBuffer.hpp>
 
+enum class SoundResourceName
+{
+	bonk,
+	break_,
+	button_press,
+	gameover,
+	miu,
+	mjau,
+	monster_ded,
+	mouse_hit_wall,
+	mouse_mus_mus,
+	movetiles_1,
+	movetiles_2,
+	movetiles_3,
+	o_oui,
+	peepeep,
+	togglebutton_1,
+	togglebutton_2
+};
+
 struct Resources
 {
 	static Resources &getResources()
@@ -16,7 +36,7 @@ struct Resources
 	std::unordered_map<int, sf::Texture> tileTextures;
   sf::Font font;
 
-	std::unordered_map<uint32_t, sf::SoundBuffer> soundEffects;
+	std::unordered_map<SoundResourceName, sf::SoundBuffer> soundEffects;
 
 private:
 	Resources();
