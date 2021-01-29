132a21b6296c5440e31eaa8e340c7895c19a1426
Raattis <raattis@gmail.com>
2021-01-30 00:54:45 +0200

    call me the cmaster

--- src/GridViz.cpp
+++ src/GridViz.cpp
@@ -5,7 +5,7 @@
 #include "GuiRenderInfo.h"
 #include "Resources.h"
 
-void GridViz::render(Grid &grid, sf::Vector2f pos, sf::Vector2f size, sf::Vector2u moveThisRow, float moveAmount)
+void GridViz::render(Grid &grid, sf::Vector2f pos, sf::Vector2f size)
 {
 	sf::Vector2f tileSize = size / float(GridSize);
 
