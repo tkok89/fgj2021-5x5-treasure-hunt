7b622332ccf6111d270ebfe11e9b082cec5c627e
Tomi Kokkonen <tomi.kokkonen+knome@gmail.com>
2021-01-30 16:29:05 +0200

    Add players files

---
+++ src/Players.cpp
@@ -0,0 +1,9 @@
+//
+//  Players.cpp
+//  fgj2021-lost-and-found
+//
+//  Created by Tomi Kokkonen on 30.1.2021.
+//  Copyright © 2021 Team Manilla. All rights reserved.
+//
+
+#include "Players.hpp"
---
+++ src/Players.hpp
@@ -0,0 +1,14 @@
+//
+//  Players.hpp
+//  fgj2021-lost-and-found
+//
+//  Created by Tomi Kokkonen on 30.1.2021.
+//  Copyright © 2021 Team Manilla. All rights reserved.
+//
+
+#ifndef Players_hpp
+#define Players_hpp
+
+#include <stdio.h>
+
+#endif /* Players_hpp */
