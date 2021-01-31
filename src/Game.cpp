525aa6dad04a959566b30abbf4cb95725fca4172
Raattis <raattis@gmail.com>
2021-01-31 15:00:59 +0200

    puhumme nyt kielillä

--- src/Game.cpp
+++ src/Game.cpp
@@ -115,6 +115,39 @@ void Game::update(sf::Time elapsedTime)
 	}
 }
 
+static void encodeAnsi(sf::Uint32 codepoint, std::back_insert_iterator<std::string> output, char replacement = '?', const std::locale &locale = std::locale())
+{
+    // NOTE: Copied from SFML/System/Utf.inl Utf<32>::encodeAnsi to avoid template errors
+    
+    // On Windows, gcc's standard library (glibc++) has almost
+    // no support for Unicode stuff. As a consequence, in this
+    // context we can only use the default locale and ignore
+    // the one passed as parameter.
+
+#if defined(SFML_SYSTEM_WINDOWS) &&                       /* if Windows ... */                          \
+    (defined(__GLIBCPP__) || defined (__GLIBCXX__)) &&     /* ... and standard library is glibc++ ... */ \
+    !(defined(__SGI_STL_PORT) || defined(_STLPORT_VERSION)) /* ... and STLPort is not used on top of it */
+
+    (void)locale; // to avoid warnings
+
+    char character = 0;
+    if (wctomb(&character, static_cast<wchar_t>(codepoint)) >= 0)
+        *output++ = character;
+    else if (replacement)
+        *output++ = replacement;
+
+    return output;
+
+#else
+
+    // Get the facet of the locale which deals with character conversion
+    const std::ctype<wchar_t> &facet = std::use_facet< std::ctype<wchar_t> >(locale);
+
+    // Use the facet to convert each character of the input string
+    *output++ = facet.narrow(static_cast<wchar_t>(codepoint), replacement);
+#endif
+}
+
 void Game::textInput(sf::Uint32 unicode)
 {
     if (unicode == 8) // backspace
@@ -130,8 +163,7 @@ void Game::textInput(sf::Uint32 unicode)
         return;
     }
 
-    sf::String string(unicode);
-    g_currentTextInput += string;
+    encodeAnsi(unicode, std::back_inserter(g_currentTextInput), '?');
 }
 
 static  std::string stateString;
